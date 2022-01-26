d4553760bf2a8d24f2efa11e60ae4d670c9c9480
HarukiHirata <suwa3217@gmail.com>
2022-01-26 18:34:16 +0900

    20220126一時コミット

--- sample.js
+++ sample.js
@@ -2,14 +2,28 @@ new Vue({
     el: '#app',
     data: {
         cards: [],
-        current_card: "",
-        img_src: "",
+        current_card: [],
+        img_src1: "",
+        img_src2: "",
+        img_src3: "",
+        img_src4: "",
+        img_src5: "",
+        isBorder1: false,
+        isBorder2: false,
+        isBorder3: false,
+        isBorder4: false,
+        isBorder5: false,
+        gameStep: 1,
     },
     methods: {
-        gameStart: function () {
+        drawCard: function () {
             this.cards = [];
             this.current_card = [];
-            this.img_src = [];
+            this.img_src1 = "";
+            this.img_src2 = "";
+            this.img_src3 = "";
+            this.img_src4 = "";
+            this.img_src5 = "";
             for (var i = 1; i < 14; i++) {
                 var cardName = 'spade_' + String(i).padStart(2,"0");
                 this.cards.push(cardName);
@@ -26,12 +40,48 @@ new Vue({
                 var cardName = 'club_' + String(l).padStart(2,"0");
                 this.cards.push(cardName);
             }
-        },
-        drawCard: function () {
             this.cards = _.shuffle(this.cards);
-            this.current_card = this.cards[0];
-            this.img_src = 'image/card_' + this.current_card + '.png';
-            this.cards.splice(0,1);
-        }
+            this.current_card = this.cards.slice(0,5);
+            this.img_src1 = "image/card_" + this.current_card[0] + ".png";
+            this.img_src2 = "image/card_" + this.current_card[1] + ".png";
+            this.img_src3 = "image/card_" + this.current_card[2] + ".png";
+            this.img_src4 = "image/card_" + this.current_card[3] + ".png";
+            this.img_src5 = "image/card_" + this.current_card[4] + ".png";
+            this.cards.splice(0,5);
+            this.gameStep = 2;
+        },
+        borderChange1: function () {
+            this.isBorder1 = !this.isBorder1;
+        },
+        borderChange2: function () {
+            this.isBorder2 = !this.isBorder2;
+        },
+        borderChange3: function () {
+            this.isBorder3 = !this.isBorder3;
+        },
+        borderChange4: function () {
+            this.isBorder4 = !this.isBorder4;
+        },
+        borderChange5: function () {
+            this.isBorder5 = !this.isBorder5;
+        },
+        cardChange: function () {
+            var card1 = document.getElementById('card1');
+            var card2 = document.getElementById('card2');
+            var card3 = document.getElementById('card3');
+            var card4 = document.getElementById('card4');
+            var card5 = document.getElementById('card5');
+            this.gameStep = 3;
+        },
+        showResult: function () {
+            this.gameStep = 1;
+            this.cards = [];
+            this.current_card = [];
+            this.img_src1 = "";
+            this.img_src2 = "";
+            this.img_src3 = "";
+            this.img_src4 = "";
+            this.img_src5 = "";
+        },
     },
 })
